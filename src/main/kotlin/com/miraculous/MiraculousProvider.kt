0714c0b3296074afcda766fb33cd8caed730ddd0
Zack-Einz <zackeinz14@gmail.com>
2025-08-29 07:31:38 -0400

    Update MiraculousProvider.kt

--- src/main/kotlin/com/miraculous/MiraculousProvider.kt
+++ src/main/kotlin/com/miraculous/MiraculousProvider.kt
@@ -1,1 +1,21 @@
+package com.miraculous
 
+import com.lagradost.cloudstream3.*
+
+class MiraculousProvider : MainAPI() {
+    override var mainUrl = "https://miraculous.to"
+    override var name = "Miraculous"
+    override val supportedTypes = setOf(TvType.Cartoon)
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        return listOf(
+            TvSeriesSearchResponse(
+                "Ejemplo Miraculous",
+                mainUrl,
+                this.name,
+                TvType.Cartoon,
+                false
+            )
+        )
+    }
+}
